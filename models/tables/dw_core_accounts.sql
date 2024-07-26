5924f5e75f8bf1f0a6ca75173a28086ebf86647a
Nithin Kumar K R <84928494+NithinKumarKR@users.noreply.github.com>
2024-07-26 14:41:01 +0530

    Update dw_core_accounts.sql

--- models/tables/dw_core_accounts.sql
+++ models/tables/dw_core_accounts.sql
@@ -1,15 +1,18 @@
 {{ config(
-    materialized='decremental',
-    unique_key = 'pkey',
-    incremental_stategy = 'merge',
-    on_schema_change= 'append_new_columns'
+    materialized='incremental',
+    unique_key='pkey',
+    incremental_strategy='merge',
+    on_schema_change='append_new_columns',
+    pre_hook="DELETE FROM mac.dw.dw_core_accounts WHERE date(last_modified_date) < date(CURRENT_DATE - interval '3 months')"
 ) }}
--- Fetch unique keys from the last 3 months
 
+-- Fetch unique keys from the last 3 months
 SELECT * 
 FROM "mac"."dw"."dw_core_accounts"
 WHERE date(last_modified_date) >= date(CURRENT_DATE - interval '3 months')
-and  TO_TIMESTAMP(SUBSTRING(cast(_airbyte_extracted_at as varchar), 1,23),'YYYY-MM-DD HH24:MI:SS.MS') > 
-        COALESCE((select max(TO_TIMESTAMP(SUBSTRING(cast(_airbyte_extracted_at as varchar), 1,23),'YYYY-MM-DD HH24:MI:SS.MS'))
-        from {{this}} ),'10000-07-19 07:40:24.709+00')
-        
+  AND TO_TIMESTAMP(SUBSTRING(cast(_airbyte_extracted_at as varchar), 1, 23), 'YYYY-MM-DD HH24:MI:SS.MS') > 
+      COALESCE(
+          (SELECT MAX(TO_TIMESTAMP(SUBSTRING(cast(_airbyte_extracted_at as varchar), 1, 23), 'YYYY-MM-DD HH24:MI:SS.MS'))
+           FROM {{ this }}),
+           '10000-07-19 07:40:24.709+00'
+      )
